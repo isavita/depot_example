0d3995c7b56e57844533e0747cc5d35406e85c60
Aleksandar <isavitaisa@gmail.com>
2014-08-14 16:52:54 +0300

    Init commit

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+Product.create!(
+  title: 'CoffeeScript',
+  description: 'The golden rule of CoffeeScript is: It\'s just JavaScript.',
+  image_url: 'http://martabakang.us/wp-content/uploads/2013/07/coffe.png',
+  price: 21.00
+)
+
+Product.create!(
+  title: 'Ruby',
+  description: 'Ruby is programming language.',
+  image_url: 'https://www.udemy.com/blog/wp-content/uploads/2012/01/rubylang.png',
+  price: 12.89
+)
