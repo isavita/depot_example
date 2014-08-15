443c9b831833aa3f84baff10b710595ca037633a
Aleksandar <isavitaisa@gmail.com>
2014-08-15 18:05:01 +0300

    Add two tests

--- Gemfile
+++ Gemfile
@@ -43,3 +43,7 @@ gem 'slim-rails', '~> 2.1.5'
 gem 'bootstrap-sass', '~> 3.2.0'
 
 gem 'autoprefixer-rails'
+
+group :development, :test do
+  gem 'rspec-rails', '~> 3.0.0'
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, uniqueness: true, length: { in: 3..80 }
+  validates :description, length: { minimum: 10 }  
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, allow_blank: true, format: {
+    with: /[.](png|jpg|dif)\z/i,
+    message: "The URL need to end with .png, .jpg, or .gif."
+  }
 end
